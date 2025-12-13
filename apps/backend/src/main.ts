a09ffd5c55644fbc0ff0d90f3e7193050bed2824
Chloe Ha <chloeha.tech@gmail.com>
2025-12-13 14:45:46 +0100

    CI/CD Backend

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api', { exclude: ['health'] });
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
 
